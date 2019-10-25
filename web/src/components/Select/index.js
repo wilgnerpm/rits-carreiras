d30b029e737bfeee85a2e8ebc034ee11062d46a5
Wilgner Pinheiro <wilgnerp@hotmail.com>
2019-10-25 17:26:04 -0300

    web: component mudanca do input. add component select

---
+++ web/src/components/Select/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState, useRef } from "react"; // add the hooks
+import { Select, useField } from "@rocketseat/unform"; // useField hook
+import {Label} from './styles'
+
+export default function SelectItem({ name, label, ...rest }) {
+   const ref = useRef(null); // for ref manipulation purposes
+  const { fieldName, registerField, defaultValue, error } = useField(name); // the name of the prop in form object is used here
+  const [selectedField, setSelectedField] = useState(defaultValue); // the state of our datepicker component
+  useEffect(() => {
+    registerField({ // here, we're registering the field in the whole form
+      name: fieldName,
+      ref: ref.current,
+      path: "props.selected", // this is the path to selected date in ReactDatepicker (wich is the selected prop)
+    });
+  }, [fieldName]);
+
+  return (
+    <>
+     <Label  >
+      {label}
+         <Select
+        name={fieldName}
+        onChange={date => setSelectedField(date)}
+        ref={ref}
+          error={true}
+        {...rest}
+
+      />
+        </Label>
+      {error && <span>{error}</span>}
+    </>
+  );
+}
