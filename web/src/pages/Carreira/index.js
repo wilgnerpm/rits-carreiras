228f0a3a900f54e74f75ed33b8aea12a1e2ae5b5
Wilgner Pinheiro <wilgnerp@hotmail.com>
2019-10-25 17:22:21 -0300

    web: page carreira

---
+++ web/src/pages/Carreira/index.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import {  Form, FileInput} from "@rocketseat/unform"; // useField hook
+import  Input from '../../components/Input'
+import  Select from '../../components/Select'
+import { ContainerBackgroud, FormCanidatese } from './styles';
+import { Container, Row, Column } from '../../components/Grid';
+import { MdCheckCircle, MdAttachFile } from 'react-icons/md';
+export default function Carreiras() {
+  const options = [
+  { id: 'Básico', title: 'Básico' },
+  { id: 'Intermedário', title: 'Intermedário' },
+  { id: 'Avançado', title: 'Avançado' },
+  { id: 'Fluente', title: 'Fluente' },
+];
+  return (
+    <>
+    <ContainerBackgroud>
+
+             <header>
+        <h2>
+Natal/RN - Brasil
+        </h2>
+        <h1>
+          Desenvolvedor PHP
+        </h1>
+      </header>
+
+
+
+            <div className='candidate'>
+ <a href="#candidate-se" className="button-green ">Candidate-se</a>
+            </div>
+
+           <div className="carreira-detalhes">
+            <div  className='carreira-item'>
+          <strong>
+Na função de Desenvolvedor Frontend aqui na Rits, você vai:
+        </strong>
+        <p>
+            <MdCheckCircle color="#4EEF61" size={14} /> Transformar layouts (XD e Photoshop) em montagens responsivas utilizando HTML + CSS + JS;
+ </p>
+  <p>
+ <MdCheckCircle color="#4EEF61" size={14} /> Integrar montagem com APIs desenvolvidas por outras equipes;
+</p>
+<p>
+ <MdCheckCircle color="#4EEF61" size={14} /> Manter e melhorar a base de código existente corrigindo bugs e refatorando código quando necessário.
+       </p>
+       </div>
+            <div  className='carreira-item'>
+          <strong>
+Procuramos alguém que:
+        </strong>
+        <p>
+            <MdCheckCircle color="#4EEF61" size={14} />
+            Possua habilidades arquiteturais para desenvolvimento de software;
+            </p>
+  <p>
+ <MdCheckCircle color="#4EEF61" size={14} />Goste de trabalhar em equipe;
+</p>
+<p>
+ <MdCheckCircle color="#4EEF61" size={14} /> Seja focado, proativo, tenha boa comunicação e relacionamento interpessoal.
+       </p>
+       </div>
+
+            <div  className='carreira-item'>
+          <strong>
+Procuramos alguém que:
+        </strong>
+        <p>
+            <MdCheckCircle color="#4EEF61" size={14} />
+            Possua habilidades arquiteturais para desenvolvimento de software;
+            </p>
+  <p>
+ <MdCheckCircle color="#4EEF61" size={14} />Goste de trabalhar em equipe;
+</p>
+<p>
+ <MdCheckCircle color="#4EEF61" size={14} /> Seja focado, proativo, tenha boa comunicação e relacionamento interpessoal.
+       </p>
+       </div>
+       </div>
+
+
+    </ContainerBackgroud>
+       <Container >
+         <Row>
+          <Column mobile={12} table={12} desktop={12} >
+            <FormCanidatese>
+                 <Form className=''>
+                   <h2 id='candidate-se'>Informações pessoais</h2>
+ <Input name="name" label="NOME COMPLETO" type="text"  placeholder=" SEU NOME"/>
+  <Input name="email" label="E-MAIL" type="email"  placeholder=" SEU E-MAIL"/>
+  <Input name="telefone" label="TELEFONE(COM DD)" type="text"  placeholder="SEU TELEFONE"/>
+    <h2>Carta de apresentação</h2>
+ <Input name="resumo" multiline label="CONTE SUA MOTIVAÇÃO (opcional)" type="text"  placeholder="Faça um breve resumo sobre você"/>
+
+    <h2>Últimas perguntas</h2>
+ <Input name="Linkedin" label="URL do seu Linkedin" type="text"  placeholder="www.linkedin.com/xxxxxxxxxx"/>
+ <Input name="github" label='URL DO SEU GITHUB' type="text"  placeholder="www.github.com/xxxxxxxxxx"/>
+
+  <Select name="tech" label='QUAL SEU NÍVEL DE INGLÊS?' options={options} />
+
+  <Input name="pretensao_salarial" label='PRETENSÃO SALARIAL' type="text" value='R$' placeholder="R"/>
+ <h2>Anexe seu currículo em PDF ou DOC</h2>
+<div class="upload-btn-wrapper">
+  <button class="btn">
+    <MdAttachFile color="#2E2236" size={14} /> Escolha um arquivo</button>
+<FileInput name="attach"  id="file"  />
+</div>
+  <button className='button-green' type="submit" >
+         ENVIAR
+        </button>
+    </Form>
+            </FormCanidatese>
+
+          </Column>
+          </Row>
+       </Container>
+       </>
+  );
+}
---
+++ web/src/pages/Carreira/styles.js
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import image1 from '../../assets/image-1.png';
+export const ContainerBackgroud = styled.div`
+  background-image: url(${image1}) ;
+  background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: auto;
+margin-top:-120px;
+align-items:center;
+padding-top:120px;
+padding-bottom:100px;
+flex:1;
+.candidate{
+margin: 45px 0;
+width:100%;
+text-align:center;
+a{
+  text-transform:uppercase;
+}
+}
+.carreiras-detalhes{
+
+}
+.carreiras-item{
+  color:#fff;
+  font-size:18px;
+  text-align:left;
+  padding:0 30px;
+@media only screen and (max-width:600px){
+  width:100%;
+}
+@media only screen and (min-width:768px){
+  width:100%;
+}
+@media only screen and (max-width:900px){
+  width:33%;
+}
+  strong{
+
+  }
+  p{
+  font-size:16px;
+  margin-top:30px;
+  }
+}
+header{
+text-align:center;
+width:100%;
+}
+h1{
+font-size:42px;
+color:#fff;
+}
+h2{
+  font-size:12px;
+color:#4EEF61;
+text-transform:uppercase
+}
+
+`;
+export const FormCanidatese = styled.div`
+max-width:560px;
+margin: 0 auto 100px;
+h2{
+  margin-top:50px;
+  margin-bottom:30px;
+}
+.upload-btn-wrapper {
+  position: relative;
+  overflow: hidden;
+  display: inline-block;
+  margin-bottom:50px;
+  margin-top:30px;
+}
+.btn {
+  border: 2px solid #4EEF61;
+  color: #2E2236;
+  background-color: white;
+  padding: 20px 110px;
+  border-radius: 40px;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.upload-btn-wrapper input[type=file] {
+  font-size: 100px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  opacity: 0;
+}
+`
