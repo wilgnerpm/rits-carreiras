df42bba651b49c8bae19360c201fb324b28e87de
Wilgner Pinheiro <wilgnerp@hotmail.com>
2019-10-25 17:25:19 -0300

    web: component Input

---
+++ web/src/pages/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Label = styled.div`
+      background: #F6F6F7;
+      display: flex;
+      flex-direction: column;
+      text-align:left;
+      padding: 9px 20px;
+      font-size:10px;
+      color:#6C6472;
+      border-radius: 4px;
+      margin-bottom:10px;
+      input, textarea {
+      background: #F6F6F7;
+      border: 0;
+      color: #2E2236;
+      margin: 5px 0 5px;
+      &::placeholder {
+        color: #D1D0D1;
+      }
+      textarea{
+        max-width: 100%;
+      }
+    }
+    span{
+      display:none
+    }
+`;
