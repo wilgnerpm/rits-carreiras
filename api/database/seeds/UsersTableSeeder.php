543873ccf4e3f2d0f62d652535d00fde2a842b7e
Wilgner Pinheiro <wilgnerp@hotmail.com>
2019-10-29 00:17:18 -0300

    criacao de admin pelo por comando seed

---
+++ api/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@rits.dev',
+            'password' => bcrypt('admin'),
+            'admin' => true,
+        ]);
+    }
+}
